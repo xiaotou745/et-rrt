673babc7199ad4261d6366cd5c7c9b8d63631e4a
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-09-28 10:24:37 +0800

    修改符合接口的JSON返回

--- renrenapihttp/src/main/java/com/renrentui/apihttp/service/inter/IUsercService.java
+++ renrenapihttp/src/main/java/com/renrentui/apihttp/service/inter/IUsercService.java
@@ -1,8 +1,13 @@
 package com.renrentui.apihttp.service.inter;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import com.renrentui.apihttp.common.HttpResultModel;
 
 /**
  * C端用户模块
@@ -13,5 +18,22 @@ import javax.ws.rs.Produces;
 @Consumes("application/json")//当前方法接收的参数类型
 @Produces("application/json; charset=utf-8")//当前类的所有方法都返回json格式的数据
 public interface IUsercService {
-
+	/**
+	 * C端忘记密码
+	 * @author 茹化肖
+	 * @date 2015年9月28日10:18:57
+	 * @return
+	 */
+	@POST
+	@Path("/forgotpwd")
+	 public HttpResultModel<Object> forgotPwd();
+	/**
+	 * C端忘记密码
+	 * @author 茹化肖
+	 * @date 2015年9月28日10:18:57
+	 * @return
+	 */
+	@POST
+	@Path("/modifypwd")
+	 public HttpResultModel<Object> modifyPwd();
 }
