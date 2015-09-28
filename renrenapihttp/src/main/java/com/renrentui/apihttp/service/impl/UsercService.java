53a10793db9c54adebd0ab47733f04fb6fd64ecb
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-09-28 10:14:53 +0800

    添加C端用户模块

---
+++ renrenapihttp/src/main/java/com/renrentui/apihttp/service/impl/UsercService.java
@@ -0,0 +1,14 @@
+package com.renrentui.apihttp.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.renrentui.apihttp.service.inter.IUsercService;
+/**
+ * 用户相关 
+ * @author 茹化肖
+ * @date 2015年9月28日09:59:42
+ */
+@Service
+public class UsercService implements IUsercService{
+
+}
