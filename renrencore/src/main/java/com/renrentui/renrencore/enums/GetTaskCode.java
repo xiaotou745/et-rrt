72ea48fbe8511a56e0a5afd876f0d94bac0f02ff
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-09-29 17:32:40 +0800

    添加领取任务接口

---
+++ renrencore/src/main/java/com/renrentui/renrencore/enums/GetTaskCode.java
@@ -0,0 +1,40 @@
+package com.renrentui.renrencore.enums;
+/**
+ * 领取任务返回值
+ * @author ofmyi_000
+ *
+ */
+public enum GetTaskCode {
+	/**
+	 * 成功
+	 */
+	Success(200, "success"),
+	UserIdErr(1141, "用户ID错误"),
+	TaskIdErr(1142, "任务ID错误"),
+	TaskExpire(1143, "任务过期或不存在"),
+	TaskHad(1144, "该任务已领取且尚未完成"),
+	TaskNoBlance(1145, "任务余量不足"),
+	Fail(1146, "系统错误");
+	private int value = 0;
+	private String desc;
+	private GetTaskCode(int value, String desc) { 
+		this.value = value;
+		this.desc = desc;
+	}
+	public int value() {
+		return this.value;
+	}
+	public String desc() {
+		return this.desc;
+	}
+
+	public static GetTaskCode getEnum(int index) {
+		for (GetTaskCode c : GetTaskCode.values()) {
+			if (c.value() == index) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+}
