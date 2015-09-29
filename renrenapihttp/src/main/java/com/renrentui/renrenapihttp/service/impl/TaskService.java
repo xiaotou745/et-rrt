081a7f53be1218cb2eff02cecd7fedd20150acb9
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-09-29 12:54:34 +0800

    task

--- renrenapihttp/src/main/java/com/renrentui/renrenapihttp/service/impl/TaskService.java
+++ renrenapihttp/src/main/java/com/renrentui/renrenapihttp/service/impl/TaskService.java
@@ -8,6 +8,7 @@ import com.renrentui.renrenapi.service.inter.IClienterService;
 import com.renrentui.renrenapihttp.common.HttpResultModel;
 import com.renrentui.renrenapihttp.service.inter.ITaskService;
 import com.renrentui.renrencore.cache.redis.RedisService;
+import com.renrentui.renrenentity.domain.TaskDetail;
 import com.renrentui.renrenentity.req.TaskDetailReq;
 
 /**
@@ -30,7 +31,7 @@ public class TaskService implements ITaskService{
 	 * C端任务详情接口
 	 */
 	@Override
-	public HttpResultModel<Object> taskDeatil(TaskDetailReq req) {
+	public HttpResultModel<TaskDetail> taskDeatil(TaskDetailReq req) {
 		// TODO Auto-generated method stub
 		return null;
 	}
---
+++ renrenentity/src/main/java/com/renrentui/renrenentity/domain/ControlInfo.java
@@ -0,0 +1,51 @@
+package com.renrentui.renrenentity.domain;
+/**
+ * 任务详情==模板 ==模板控件实体类
+ * @author ofmyi_000
+ *
+ */
+public class ControlInfo {
+
+	private String controlType;
+	private String title;
+	private String name;
+	private String defaultValue;
+	private String controlData;
+	private int OrderNum;
+	public String getControlType() {
+		return controlType;
+	}
+	public void setControlType(String controlType) {
+		this.controlType = controlType;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+	public String getControlData() {
+		return controlData;
+	}
+	public void setControlData(String controlData) {
+		this.controlData = controlData;
+	}
+	public int getOrderNum() {
+		return OrderNum;
+	}
+	public void setOrderNum(int orderNum) {
+		OrderNum = orderNum;
+	}
+}
