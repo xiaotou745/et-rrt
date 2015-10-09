6e7d312e24c3b2ed391c27f682f8102cbb95d492
caoheyang <cao.heyang@etaostars.com>
2015-10-09 18:00:39 +0800

    任务

---
+++ renrenservice/src/main/java/com/renrentui/renrenservice/service/TaskWindowService.java
@@ -0,0 +1,18 @@
+package com.renrentui.renrenservice.service;
+
+/**
+ * 人人地推任务相关服务
+ * @author CaoHeYang
+ * @date 20151009
+ */
+public class TaskWindowService {
+	/**
+	 * 超市取消任务服务
+	 * 
+	 * @author CaoHeYang
+	 * @date 20151009
+	 */
+	public void OutTimeCanelTask() {
+
+	}
+}
