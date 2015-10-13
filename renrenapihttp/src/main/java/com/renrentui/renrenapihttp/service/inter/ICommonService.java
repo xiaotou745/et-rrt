e1a4220da00455aaf9ca70307502978935e384d3
caoheyang <cao.heyang@etaostars.com>
2015-10-13 15:35:00 +0800

    人人

--- renrenapihttp/src/main/java/com/renrentui/renrenapihttp/service/inter/ICommonService.java
+++ renrenapihttp/src/main/java/com/renrentui/renrenapihttp/service/inter/ICommonService.java
@@ -1,12 +1,8 @@
 package com.renrentui.renrenapihttp.service.inter;
-import java.util.List;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 import com.renrentui.renrenapihttp.common.HttpResultModel;
 import com.renrentui.renrenentity.AppVersion;
 import com.renrentui.renrenentity.req.VersionCheckReq;
