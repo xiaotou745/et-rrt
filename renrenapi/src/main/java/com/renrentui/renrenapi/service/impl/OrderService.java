77312fec144717ba825aa3821dff04e3e5ff4701
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-10-14 14:32:26 +0800

    取消订单

--- renrenapi/src/main/java/com/renrentui/renrenapi/service/impl/OrderService.java
+++ renrenapi/src/main/java/com/renrentui/renrenapi/service/impl/OrderService.java
@@ -10,12 +10,17 @@ import com.renrentui.renrenapi.dao.inter.IClienterBalanceDao;
 import com.renrentui.renrenapi.dao.inter.IClienterBalanceRecordDao;
 import com.renrentui.renrenapi.dao.inter.IOrderDao;
 import com.renrentui.renrenapi.dao.inter.IOrderLogDao;
+import com.renrentui.renrenapi.dao.inter.IRenRenTaskDao;
 import com.renrentui.renrenapi.service.inter.IOrderService;
+import com.renrentui.renrencore.enums.CancelTaskCode;
 import com.renrentui.renrenentity.ClienterBalanceRecord;
 import com.renrentui.renrenentity.OrderLog;
 import com.renrentui.renrenentity.common.PagedResponse;
+import com.renrentui.renrenentity.domain.CheckCancelOrder;
 import com.renrentui.renrenentity.domain.OrderAudit;
 import com.renrentui.renrenentity.domain.OrderChildInfoModel;
+import com.renrentui.renrenentity.req.CancelOrderReq;
+import com.renrentui.renrenentity.req.CancelTaskReq;
 import com.renrentui.renrenentity.req.ClienterBalanceReq;
 import com.renrentui.renrenentity.req.OrderAuditReq;
 import com.renrentui.renrenentity.req.OrderChildReq;
@@ -30,7 +35,8 @@ public class OrderService implements IOrderService{
 	private IClienterBalanceDao clienterBalanceDao;
 	@Autowired
 	private IClienterBalanceRecordDao clienterBalanceRecordDao;
-	
+	@Autowired
+	private IRenRenTaskDao renRenTaskDao;	
 	/**
 	 * 获取订单审核列表
 	 * 茹化肖
@@ -178,5 +184,36 @@ public class OrderService implements IOrderService{
 		//拼接内容结束
 		sBuilder.append("</body></html>");
 		return sBuilder.toString();
+	}
+	/**
+	 * 手动取消订单
+	 * 茹化肖
+	 * 2015年10月14日13:51:23
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class, timeout = 30)
+	public int cancelOrder(CancelTaskReq req) {
+		//验证订单相关信息
+		CheckCancelOrder check=orderDao.checkCancelOrder(req);
+		if(check==null)//订单不存在
+			return CancelTaskCode.OrderNull.value();
+		int res=orderDao.cancelOrder(req);//取消订单
+		String optName=req.getRemark();
+		OrderLog orderLog=new OrderLog();
+		orderLog.setOrderNo(check.getOrderNo());
+		orderLog.setOrderId(req.getOrderId());
+		orderLog.setOptType(Short.valueOf("1"));
+		orderLog.setOptName(optName);
+		orderLog.setRemark(optName+"取消订单:"+check.getOrderNo());
+		int orderlogres=orderLogDao.addOrderLog(orderLog);//记录订单操作日志
+		
+		int addres=renRenTaskDao.addTaskAvailableCount(check.getTaskId());//增加任务的剩余量
+		if(res>0&&orderlogres>0&&addres>0){
+			return CancelTaskCode.Success.value();
+		}
+		else {
+			Error error=new Error("取消任务错误");
+			throw new RuntimeErrorException(error);
+		}
 	}
 }
