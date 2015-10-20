4ecaf2a9312fd15ad58bf41efaf903a7f395784c
赵海龙 <zhao.hailong@etao.cn>
2015-10-20 13:09:05 +0800

    apihttp项目添加actionlog，为了以后监控

--- renrenapihttp/src/main/java/com/renrentui/renrenapihttp/common/AESInterceptor.java
+++ renrenapihttp/src/main/java/com/renrentui/renrenapihttp/common/AESInterceptor.java
@@ -3,14 +3,12 @@ package com.renrentui.renrenapihttp.common;
 import java.io.InputStream;
 import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.Message; 
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
-import org.apache.cxf.transport.http.AbstractHTTPDestination;
+
 
 import com.renrentui.renrencore.security.AES;
 import com.renrentui.renrencore.util.JsonUtil;
@@ -18,8 +16,6 @@ import com.renrentui.renrencore.util.PropertyUtils;
 import com.renrentui.renrencore.util.StreamUtils;
 import com.renrentui.renrencore.util.StringUtils;
 import com.renrentui.renrenentity.req.AesParameterReq;
-
-
 public class AESInterceptor  extends AbstractPhaseInterceptor<Message> {
 
 	public AESInterceptor() {
@@ -31,17 +27,22 @@ public class AESInterceptor  extends AbstractPhaseInterceptor<Message> {
 	public void handleMessage(Message message) throws Fault {
 		String encryptMsg="";
 		String decryptMsg="";
+		logCustomerInfo(message,encryptMsg,decryptMsg);
 		InputStream mContentString = message.getContent(InputStream.class);
 		String inputMsg = StreamUtils.copyToStringNoclose(mContentString);
+		logCustomerInfo(message,inputMsg,decryptMsg);
+		
 		AesParameterReq req = JsonUtil.str2obj(inputMsg,AesParameterReq.class);
 		encryptMsg=req.getData();
 		decryptMsg=req.getData();
+		logCustomerInfo(message,encryptMsg,decryptMsg);
 		String interceptSwith =PropertyUtils.getProperty("InterceptSwith");//"1" 开启加密
 		if(interceptSwith.equals("1"))
 		{
 			System.out.println("已开启AES解密拦截器");
 			try {
 				decryptMsg = AES.aesDecrypt(StringUtils.trimRight(req.getData(),"\n"));// AES解密
+				logCustomerInfo(message,encryptMsg,decryptMsg);
 				InputStream stream=StreamUtils.StringToInputStream(decryptMsg);
 				message.setContent(InputStream.class, stream);//回填流
 			} catch (Exception e) {
@@ -52,10 +53,12 @@ public class AESInterceptor  extends AbstractPhaseInterceptor<Message> {
 		{
 			System.out.println("暂未开启AES解密拦截器");
 		}
+		System.out.println("未解密的入参:"+encryptMsg);
+		System.out.println("解密后的入参:"+decryptMsg);
 		logCustomerInfo(message,encryptMsg,decryptMsg);
 	}
 	/**
-	 * 记录额外的信息，用于统计log
+	 * 记录额外的信息，用于统计log（先删除，后添加）
 	 * @author hailongzhao
 	 * @date 20151019
 	 * @param message
@@ -64,10 +67,17 @@ public class AESInterceptor  extends AbstractPhaseInterceptor<Message> {
 	 */
 	private void logCustomerInfo(Message message,String encryptMsg,String decryptMsg){
 		Exchange exchange = message.getExchange();
+		if (exchange.containsKey("requestTime")) {
+			exchange.remove("requestTime");
+		}
+		if (exchange.containsKey("encryptMsg")) {
+			exchange.remove("encryptMsg");
+		}
+		if (exchange.containsKey("decryptMsg")) {
+			exchange.remove("decryptMsg");
+		}
 		exchange.put("requestTime", new Date());
 		exchange.put("encryptMsg", encryptMsg);
 		exchange.put("decryptMsg", decryptMsg);
-		System.out.println("未解密的入参:"+encryptMsg);
-		System.out.println("解密后的入参:"+decryptMsg);
 	}
 }
