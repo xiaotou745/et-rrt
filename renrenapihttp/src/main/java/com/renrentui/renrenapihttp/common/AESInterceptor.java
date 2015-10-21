c1c3c0c50ee5a6c2cec95fdcb44b7b9d3e4e29c6
赵海龙 <zhao.hailong@etao.cn>
2015-10-21 15:52:55 +0800

    测试环境，apihttp项目不加密

--- renrenapihttp/src/main/java/com/renrentui/renrenapihttp/common/AESInterceptor.java
+++ renrenapihttp/src/main/java/com/renrentui/renrenapihttp/common/AESInterceptor.java
@@ -13,6 +13,7 @@ import org.apache.cxf.phase.Phase;
 
 
 
+
 import com.renrentui.renrencore.security.AES;
 import com.renrentui.renrencore.util.JsonUtil;
 import com.renrentui.renrencore.util.PropertyUtils;
@@ -46,19 +47,23 @@ public class AESInterceptor  extends AbstractPhaseInterceptor<Message> {
 				encryptMsg = inputMsg;
 				decryptMsg = inputMsg;
 				System.out.println("暂未开启AES解密拦截器");
-				if (decryptMsg.indexOf("{") < 0 && decryptMsg.indexOf("}") < 0) {
+				if (inputMsg.indexOf("data")>0) {
 					throw new RuntimeException("传递的入参是加密后的字符串，但是apihttp项目暂未开启AES解密");
 				}
 			}
 			InputStream stream = StreamUtils.StringToInputStream(decryptMsg);
 			message.setContent(InputStream.class, stream);// 回填流
 		} catch (Exception e) {
+			logCustomerInfo(message, encryptMsg, decryptMsg);
 			throw new RuntimeException("处理入参时出错:"+e.getMessage());
 		}
 
 		System.out.println("未解密的入参:" + encryptMsg);
 		System.out.println("解密后的入参:" + decryptMsg);
 		logCustomerInfo(message, encryptMsg, decryptMsg);
+		if (decryptMsg.indexOf("{") < 0 && decryptMsg.indexOf("}") < 0) {
+			throw new RuntimeException("传递的入参是加密后的字符串，但是apihttp项目暂未开启AES解密");
+		}
 	}
 	/**
 	 * 记录额外的信息，用于统计log（先删除，后添加）
