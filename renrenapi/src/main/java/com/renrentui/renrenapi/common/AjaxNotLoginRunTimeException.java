695cee4d34a0110d660ba1e6b56154bc5459c0fd
赵海龙 <zhao.hailong@etao.cn>
2015-11-16 17:55:56 +0800

    添加ajax全局处理

---
+++ renrenapi/src/main/java/com/renrentui/renrenapi/common/AjaxNotLoginRunTimeException.java
@@ -0,0 +1,10 @@
+package com.renrentui.renrenapi.common;
+
+public class AjaxNotLoginRunTimeException extends RuntimeException{
+	public AjaxNotLoginRunTimeException(String msg) {
+		super(msg);
+	}
+	public AjaxNotLoginRunTimeException() {
+		super();
+	}
+}
