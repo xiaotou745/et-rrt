e32e095acb05c5351205522bdeaa1c64cfaece55
赵海龙 <zhao.hailong@etao.cn>
2015-11-24 14:24:45 +0800

    添加任务枚举

---
+++ renrencore/src/main/java/com/renrentui/renrencore/enums/TaskType.java
@@ -0,0 +1,43 @@
+package com.renrentui.renrencore.enums;
+
+/**
+ * 任务类型：1 签约任务 2 分享任务 3 下载任务
+ * @author hailongzhao
+ * @date 20151124
+ */
+public enum TaskType {
+	/**
+	 * 签约任务
+	 */
+	WaitAudit(1, "签约任务"),
+	/**
+	 * 分享任务
+	 */
+	Audited(2, "分享任务"),
+	/**
+	 * 下载任务
+	 */
+	Reject(3, "下载任务");
+
+	private int value = 0;
+	private String desc;
+	private TaskType(int value, String desc) { 
+		this.value = value;
+		this.desc = desc;
+	}
+	public int value() {
+		return this.value;
+	}
+	public String desc() {
+		return this.desc;
+	}
+
+	public static TaskType getEnum(int index) {
+		for (TaskType c : TaskType.values()) {
+			if (c.value() == index) {
+				return c;
+			}
+		}
+		return null;
+	}
+}
