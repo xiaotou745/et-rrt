8bf56d7fdfbdfedd8ba6b7feeeac2c8c41d8a95b
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-12-29 13:37:42 +0800

    添加分佣策略

--- renrenadmin/src/main/java/com/renrentui/renrenadmin/controller/SubCommissionController.java
+++ renrenadmin/src/main/java/com/renrentui/renrenadmin/controller/SubCommissionController.java
@@ -1,7 +1,12 @@
 package com.renrentui.renrenadmin.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.renrentui.renrenentity.RoleInfo;
 /**
  * 
  * 分佣管理
@@ -11,5 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("subcommission")
 public class SubCommissionController {
-
+	@RequestMapping("add")
+	public ModelAndView list() {
+		ModelAndView view = new ModelAndView("adminView");
+		view.addObject("subtitle", "分佣管理");
+		view.addObject("currenttitle", "添加分佣策略");
+		view.addObject("viewPath", "subcommission/add");
+		return view;
+	}
 }
---
+++ renrenadmin/src/main/webapp/WEB-INF/views/subcommission/add.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@page import="com.renrentui.renrencore.util.PropertyUtils"%>
+<%@page import="java.util.List"%>
+<%
+	String basePath = PropertyUtils.getProperty("java.renrenadmin.url");
+%>
+<div>
+添加分佣策略
+
+</div>
