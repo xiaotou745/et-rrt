dec1f7afbc825f1a2f64964626ca112c1a64621e
赵海龙 <zhao.hailong@etao.cn>
2016-02-01 14:22:04 +0800

    修改新建任务时文本框值的校验

--- renrenadmin/src/main/webapp/resources/js/renrentask.js
+++ renrenadmin/src/main/webapp/resources/js/renrentask.js
@@ -1,105 +1,104 @@
 
 //
 function CheckSave(){
-	 
-	//任务基本信息校验
-	if($('#taskTitle').val().length<5||$('#taskTitle').val().length>50)
-	{
+		// 任务基本信息校验
+	if ($('#taskTitle').val().length < 5 || $('#taskTitle').val().length > 50) {
 		alert('任务标题必须在5-50个字符之间');
 		return false;
 	}
-	if($('#taskGeneralInfo').val().length<5||$('#taskGeneralInfo').val().length>20)
-	{
+	if ($('#taskGeneralInfo').val().length < 5
+			|| $('#taskGeneralInfo').val().length > 20) {
 		alert('任务描述必须在5-20个字符之间');
 		return false;
 	}
-	if(!isInt($('#auditCycle').val()))
-	{
+	if (!isInt($('#auditCycle').val())
+			|| isNaN(parseInt($('#auditCycle').val()))||
+			parseInt($('#auditCycle').val())>2147483648) {
 		alert('审核周期必须为整数');
 		return false;
 	}
-	var reg=/^\d{11}$/;
-	if($('#hotline').val()!=''&&!reg.test($('#hotline').val()))
-	{
-		alert('咨询电话必须为11位数字');
-		return false;
-	}
-	if(parseInt($('#auditCycle').val())<1||isNaN(parseInt($('#auditCycle').val())))
-	{
+	if (parseInt($('#auditCycle').val()) < 1) {
 		alert('审核周期必须大于1天');
 		return false;
 	}
-	if(parseFloat($('#amount').val())<0.01||isNaN(parseFloat($('#amount').val())))
-	{
+	if (parseFloat($('#amount').val()) < 0.01
+			|| isNaN(parseFloat($('#amount').val()))||
+			parseInt($('#amount').val())>2147483648) {
 		alert('地推员佣金必须大于等于0.01元');
 		return false;
 	}
-	if(!isInt($('#estimatedTime').val()))
-	{
-		alert('预计完成消耗必须为整数');
-		return false;
-	}
-	if(parseInt($('#estimatedTime').val())<1||isNaN(parseInt($('#estimatedTime').val())))
-	{
-		alert('预计完成消耗必须大于等于1分钟');
-		return false;
-	}
-	if(parseFloat($('#totalAmount').val())<0.01||isNaN(parseFloat($('#totalAmount').val())))
-	{
+	if (parseFloat($('#totalAmount').val()) < 0.01
+			|| isNaN(parseFloat($('#totalAmount').val()))||
+			parseInt($('#totalAmount').val())>2147483648) {
 		alert('任务总佣金必须大于等于0.01元');
 		return false;
 	}
-	if(parseFloat($('#totalAmount').val())<parseFloat($('#amount').val()))
-	{
+	if (parseFloat($('#totalAmount').val()) < parseFloat($('#amount').val())) {
 		alert('任务总佣金必须大于等于地推员佣金');
 		return false;
-	};
-	if($('input[name="rTaskType"]:checked').val()!=1)
-	{
-		if($('#downUrl').val()=='')
-			{
+	}
+	;
+	if (!isInt($('#estimatedTime').val())
+			|| isNaN(parseInt($('#estimatedTime').val()))||
+			parseInt($('#estimatedTime').val())>2147483648) {
+		alert('预计完成消耗必须为整数');
+		return false;
+	}
+	if (parseInt($('#estimatedTime').val()) < 1) {
+		alert('预计完成消耗必须大于等于1分钟');
+		return false;
+	}
+	// 开始日期结束日期验证
+	var startDate = $('#beginDate').val();
+	var endDate = $('#endDate').val();
+	if (startDate.length < 1 || endDate.length < 1) {
+		alert('开始时间或结束时间不能为空');
+		return false;
+	}
+	var intStartDate = startDate.replace(/-/g, "");
+	var intEndDate = endDate.replace(/-/g, "");
+	if (intStartDate > intEndDate) {
+		alert('开始日期不能大于结束日期');
+		$('#beginDate').val("");
+		return false;
+	}
+	var myDate = new Date();
+	var nowdate = myDate.getFullYear()
+			+ (myDate.getMonth() < 10 ? ("0" + (myDate.getMonth() + 1)): (myDate.getMonth() + 1)
+			+ (myDate.getDate() < 10 ? ("0" + myDate.getDate()): myDate.getDate()));
+	if (parseInt(intStartDate) < parseInt(nowdate)) {
+		alert("开始日期必须大于等于今天");
+		return false;
+	}
+	if (parseInt(intEndDate) <= parseInt(nowdate)) {
+		alert("结束日期必须大于今天");
+		return false;
+	}
+	var reg = /^\d{11}$/;
+	if ($('#hotline').val() != '' && !reg.test($('#hotline').val())) {
+		alert('咨询电话必须为11位数字');
+		return false;
+	}
+
+	if ($('input[name="rTaskType"]:checked').val() != 1) {
+		if ($('#downUrl').val() == '') {
 			alert('下载链接不能为空');
 			return false;
-			}
-		if($('#downUrl').val().length>200){
-		alert('下载链接不能大于200个字符');
-		return false;
 		}
-		if($('#scanTip').val().length>20)
-		{
-		alert('扫码说明不能大于20个字符');
-		return false;
+		if ($('#downUrl').val().length > 200) {
+			alert('下载链接不能大于200个字符');
+			return false;
 		}
-		if($('#reminder').val().length>40)
-		{
-		alert('温馨提示不能大于40个字符');
-		return false;
+		if ($('#scanTip').val().length > 20) {
+			alert('扫码说明不能大于20个字符');
+			return false;
+		}
+		if ($('#reminder').val().length > 40) {
+			alert('温馨提示不能大于40个字符');
+			return false;
 		}
 	}
-	//开始日期结束日期验证
-	  var startDate = $('#beginDate').val();
-	  var endDate = $('#endDate').val();
-	  if(startDate.length<1||endDate.length<1){
-		  alert('开始时间或结束时间不能为空');
-			return false;
-		  }
-	  var intStartDate = startDate.replace(/-/g, "");
-	  var intEndDate = endDate.replace(/-/g, "");
-	  if (intStartDate > intEndDate) {
-	      alert('开始日期不能大于结束日期');
-	      $('#beginDate').val("");
-	      return false;
-	  }
-		var myDate = new Date();
-		var nowdate=myDate.getFullYear()+""+(myDate.getMonth()<10?("0"+(myDate.getMonth()+1)):(myDate.getMonth()+1)+""+(myDate.getDate()<10?("0"+myDate.getDate()):myDate.getDate()));
-		if(parseInt(intStartDate)<parseInt(nowdate)){
-			alert("开始日期必须大于等于今天");
-			return false;
-		}
-		if(parseInt(intEndDate)<=parseInt(nowdate)){
-			alert("结束日期必须大于今天");
-			return false;
-		}
+	
 		var flag=true;
 		//步骤信息不能为空
 		$('#setpbox input').each(function(id,el){
