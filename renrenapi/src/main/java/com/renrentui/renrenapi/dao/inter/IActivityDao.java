60df2fab70a2143292de321109ac4edef815e7aa
WangChao <wang.chao@etaostars.com>
2016-02-24 19:57:57 +0800

    活动管理

---
+++ renrenapi/src/main/java/com/renrentui/renrenapi/dao/inter/IActivityDao.java
@@ -0,0 +1,18 @@
+package com.renrentui.renrenapi.dao.inter;
+
+import java.util.List;
+
+import com.renrentui.renrenentity.Activity;
+import com.renrentui.renrenentity.common.PagedResponse;
+import com.renrentui.renrenentity.req.PagedActivityReq;
+import com.renrentui.renrenentity.req.UpdateActivityReq;
+
+public interface IActivityDao {
+
+	List<Activity> getList();
+
+	int shutUpActivity(UpdateActivityReq req);
+
+	int startUpActivity(UpdateActivityReq req);
+
+}
