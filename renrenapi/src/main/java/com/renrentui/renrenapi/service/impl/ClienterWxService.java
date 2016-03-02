cf04f4275de1f79680c61373a5bc40814fe7069a
douhaichao <dou631@163.com>
2016-03-02 16:28:51 +0800

    微信官网

--- renrenapi/src/main/java/com/renrentui/renrenapi/service/impl/ClienterWxService.java
+++ renrenapi/src/main/java/com/renrentui/renrenapi/service/impl/ClienterWxService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.renrentui.renrenapi.dao.impl.ClienterDao;
 import com.renrentui.renrenapi.dao.inter.IClienterWxDao;
 import com.renrentui.renrenapi.dao.inter.IClienterWxLogDao;
 import com.renrentui.renrenapi.service.inter.IClienterWxService;
@@ -24,22 +25,36 @@ public class ClienterWxService implements IClienterWxService {
 	public boolean follow(String openId, String fromUserName, String createTime) {
 
 		try {
-			ClienterWx clienterWx = new ClienterWx();
-			clienterWx.setClienterId(0);
-			clienterWx.setFollowStatus(1);
-			clienterWx.setFromUserName(fromUserName);
-			clienterWx.setOpenId(openId);
-			clienterWx.setWxId("");
-			clienterWxDao.insert(clienterWx);
-
+			ClienterWx resultClienterWx = clienterWxDao
+					.getClienterByOpenId(openId);
+			if (resultClienterWx == null || resultClienterWx.getId() <= 0) {
+				// 第一次绑定
+				ClienterWx clienterWx = new ClienterWx();
+				clienterWx.setClienterId(0);
+				clienterWx.setFollowStatus(1);
+				clienterWx.setFromUserName(fromUserName);
+				clienterWx.setOpenId(openId);
+				clienterWx.setWxId("");
+				clienterWx.setClienterId(0);
+				clienterWxDao.insert(clienterWx);
+			}
+			else if (resultClienterWx.getFollowStatus() == 1) {
+				// 当前用户已经绑定过了
+				return false;
+			} else if (resultClienterWx.getFollowStatus() == 0) {
+				// 取消后再次绑定的
+				clienterWxDao.follow(openId);
+			}
+			
 			ClienterWxLog clienterWxLog = new ClienterWxLog();
 			clienterWxLog.setFollowStatus(1);
 			clienterWxLog.setOpenId(openId);
 			clienterWxLogDao.insert(clienterWxLog);
 		} catch (Exception e) {
 			System.out.println(e);
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 	/** 取消关注 窦海超 2016年2月25日 16:00:51 */
@@ -47,7 +62,6 @@ public class ClienterWxService implements IClienterWxService {
 	@Transactional(rollbackFor = Exception.class, timeout = 30)
 	public boolean unfollow(String openId) {
 		try {
-
 			clienterWxDao.updateUnFollow(openId);
 			ClienterWxLog clienterWxLog = new ClienterWxLog();
 			clienterWxLog.setFollowStatus(0);// 取消关注
@@ -55,8 +69,9 @@ public class ClienterWxService implements IClienterWxService {
 			clienterWxLogDao.insert(clienterWxLog);
 		} catch (Exception e) {
 			System.out.println(e);
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 	@Override
